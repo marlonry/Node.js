abced0ee8b3f519a9f0c2d4a35fe05cccdc16881
unknown <marlonspr@hotmail.com>
2019-07-26 02:09:08 +1200

    How node works

---
+++ 2-how-node-works/starter/event-loop.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+const crypto = require("crypto"); //offloaded automatically to the threadpool
+const express = require("express");
+
+console.log(UV_THREADPOOL_SIZE = 5); // -> undefined
+
+const start = Date.now();
+// process.env.UV_THREADPOOL_SIZE = 1;
+
+setTimeout(() => {
+    console.log("Timer 1 finished")
+}, 0);
+
+setImmediate(() => {
+    console.log("Immediate 1 finished");
+});
+
+fs.readFile('test-file.txt', () => {
+    console.log('I/O finished');
+    console.log('------------');
+
+    setTimeout(() => {
+        console.log("Timer 2 finished")
+    }, 0);
+
+    setTimeout(() => {
+        console.log("Timer 3 finished")
+    }, 3000);
+
+    setImmediate(() => {
+        console.log("Immediate 2 finished");
+    });
+
+    process.nextTick(() => {
+        console.log("Process.NextTick()");
+    });
+
+    crypto.pbkdf2("password", 'salt', 100000, 1024, 'sha512', () => {
+        console.log(Date.now() - start, "Password encrypted");
+    });
+
+    crypto.pbkdf2("password", 'salt', 100000, 1024, 'sha512', () => {
+        console.log(Date.now() - start, "Password encrypted");
+    });
+
+    crypto.pbkdf2("password", 'salt', 100000, 1024, 'sha512', () => {
+        console.log(Date.now() - start, "Password encrypted");
+    });
+
+    crypto.pbkdf2("password", 'salt', 100000, 1024, 'sha512', () => {
+        console.log(Date.now() - start, "Password encrypted");
+    });
+
+    crypto.pbkdf2("password", 'salt', 100000, 1024, 'sha512', () => {
+        console.log(Date.now() - start, "Password encrypted");
+    });
+});
+
+console.log('Hello from the top level code');
---
+++ 2-how-node-works/starter/events.js
@@ -0,0 +1,49 @@
+const EventEmitter = require('events');
+const http = require('http');
+
+// extend from the even emitter to acess all the methods in the event emitter
+class Sales extends EventEmitter {
+    constructor() {
+        super();
+    }
+}
+
+const myEmitter = new Sales(); // subscribe and then react
+
+myEmitter.on('newSale', () => { // observer
+    console.log('There was a new sale!!!');
+});
+
+myEmitter.on('newSale', () => { // observer
+    console.log("Costumer name: Jonas!!!");
+});
+
+myEmitter.on('newSale', stock => { // observer
+    console.log(`There are now ${stock} items left in stock`);
+})
+
+myEmitter.emit('newSale', 9); // emits events // event register and then arguments for the observer to use
+
+// runs syncronously
+
+/////////////////////////////////////////
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+
+    console.log(req.url);
+
+    console.log('Request Received!');
+    res.end('Request Received');
+})
+server.on('request', (req, res) => {
+    console.log('Another Request :D');
+})
+server.on('close', () => {
+    console.log("Server closed");
+})
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Waiting for incoming request...');
+});
